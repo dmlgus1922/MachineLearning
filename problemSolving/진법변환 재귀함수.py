463d71b8d8be706f6301c6185d695f00169b2064
김의현 <dmlgus1922@naver.com>
2022-07-07 15:50:12 +0900

    진법 변환 재귀함수
    10진수를 2진수, 8진수, 16진수로 변환해 출력하는 프로그램이다. 재귀함수를 사용하였다.

---
+++ problemSolving/진법변환 재귀함수.py
@@ -0,0 +1,52 @@
+b2 = []
+o8 = []
+h16 = []
+hex_ = {10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F'}
+
+def base2(n):
+    if n == 1:
+        b2.append(1)
+        return
+    if n % 2 == 0:
+        b2.append(0)
+        return base2(n//2)
+    else:
+        b2.append(1)
+        return base2(n//2)
+
+def base8(n):
+    if n <= 7:
+        o8.append(n)
+        return
+    if n % 8 == 0:
+        o8.append(0)
+        return base8(n//8)
+    else:
+        o8.append(n%8)
+        return base8(n//8)
+
+def base16(n):
+    if n <= 9:
+        h16.append(n)
+        return
+    elif n <= 15:
+        h16.append(hex_[n])
+        return
+
+    if n % 16 == 0:
+        h16.append(0)
+        return base16(n//16)
+    else:
+        if n % 16 <= 9:
+            h16.append(n%16)
+        elif n % 16 <= 15:
+            h16.append(hex_[n%16])
+        return base16(n//16)
+
+n = int(input('10진수 입력 -->'))
+base2(n)
+base8(n)
+base16(n)
+print('2진수 :', *b2[::-1])
+print('8진수 :', *o8[::-1])
+print('16진수 :', *h16[::-1])
